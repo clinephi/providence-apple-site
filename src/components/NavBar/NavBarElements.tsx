f90509d6cd23b7126f7b87ebbfd1edbb7f20aeb5
philip.cline <philip.cline@ibigroup.com>
2023-03-05 19:37:36 -0500

    update navbar colour

--- src/components/NavBar/NavBarElements.tsx
+++ src/components/NavBar/NavBarElements.tsx
@@ -3,7 +3,8 @@ import { NavLink as Link } from 'react-router-dom'
 import {FaBars} from 'react-icons/fa'
 
 export const Nav = styled.nav`
-  background: #870d0dae;
+  background: #854030;
+  /* background: #870d0dae; */
   height: 80px;
   display: flex;
   /* justify-content: space-between; */
