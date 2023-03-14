01e091271826037a63bcd4096360755b2dabbf74
philip.cline <philip.cline@ibigroup.com>
2023-03-13 21:59:57 -0400

    add Google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactGA from "react-ga4";
+
 import './styles/App.css';
 
 import logo from './logo.svg';
@@ -9,6 +11,8 @@ import NavBar from './components/NavBar';
 
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Home } from './pages';
+import reportWebVitals from './reportWebVitals';
+
 // TODO: move all pages into one import with index.
 import { About } from './pages/About';
 import { Cultivation } from './pages/Cultivation';
@@ -16,6 +20,8 @@ import { Nutrition } from './pages/Nutrition';
 import { TreeAvailability } from './pages/TreeAvailability';
 import { FurtherInfo } from './pages/FurtherInfo'
 import { Products } from './pages/Products';
+
+ReactGA.initialize("G-538DQYHYTK")
 
 function App() {
   return (
@@ -35,5 +41,17 @@ function App() {
     </Router>
   );
 }
+
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(SendAnalytics);
 
 export default App;
